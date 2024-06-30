8b90c7603048ef11697a74b30a4e2c8a8af4630b
kormuch <57794998+kormuch@users.noreply.github.com>
2024-06-30 17:54:37 +0200

    Add files via upload

---
+++ EngineV10/ChildClass.py
@@ -0,0 +1,7 @@
+class ChildClass:
+    def __init__(self):
+        self.helloworld = "Hello world"  # Make the variable an instance variable
+
+    def returnChild(self):
+        return self.helloworld  # Use self to access the instance variable
+
---
+++ EngineV10/Main.py
@@ -0,0 +1,40 @@
+#### Zuletzt: GUI Button Quit2 mit StatusID=1 zum laufen bringen
+
+# Main class
+import pygame
+from GameEngine import ClassGameEngine
+from GUI import ClassBoardGameGUI
+
+# gui_loop.py
+import sys
+
+
+
+# Create an instance of the game engine
+engine = ClassGameEngine()
+gui = ClassBoardGameGUI()
+
+
+while engine.game_statusID != 0:
+    gui.runGUI_initialize_game()
+    gui.runGUI_game()
+
+
+# Ask for the number of players
+players = engine.initialize_game()
+
+
+while engine.game_statusID != 0:
+    if engine.game_statusID == 1:
+        pygame.init()
+        engine.deal_6_cards_perplayer()
+        engine.game_statusID = 1000
+        
+    if engine.game_statusID == 1000:
+        engine.run_level_loop()
+        
+
+    
+
+pygame.quit()
+sys.exit()
