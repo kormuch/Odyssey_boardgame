2711c8cb08bf1edc8c2c4b12185f1fd3d77fa3ff
kormuch <57794998+kormuch@users.noreply.github.com>
2025-08-16 19:27:05 +0200

    Delete v3 directory

--- v3/PlayerData.py
+++
@@ -1,6 +0,0 @@
-class Player:
-    def __init__(self, playerNumber):
-        self.playerNumber = playerNumber
-        self.actioncards = {}
-        self.actioncards_played = 0
-        self.damageSuffered_player = 0
