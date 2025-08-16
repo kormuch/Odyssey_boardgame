61df063c32a47bb07564c8eff40fcdb43b85afb4
kormuch <57794998+kormuch@users.noreply.github.com>
2025-08-16 19:34:57 +0200

    Delete EngineV10 directory

--- EngineV10/ChildClass.py
+++
@@ -1,7 +0,0 @@
-class ChildClass:
-    def __init__(self):
-        self.helloworld = "Hello world"  # Make the variable an instance variable
-
-    def returnChild(self):
-        return self.helloworld  # Use self to access the instance variable
-
